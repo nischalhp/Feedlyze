c437b9ad70d2532c9e195a394e28867769c2cec2
Nischal HP <n.hp@sap.com>
2014-01-16 23:13:12 +0530

    Adding thread to return output for each category

--- services/testClasses.py
+++ services/testClasses.py
@@ -1,6 +1,10 @@
 import readCsv
 import analyzeData
 from generatejson import JSONUtil
+import thread
+
+
+
 readObj = readCsv.CSVReader('https://docs.google.com/spreadsheet/ccc?key=0AiNZkDgFRjjOdERzLU01NTdVeE5ZaDUzbi1xNTRfSnc&usp=sharing#gid=0')
 outputDictionary = readObj.getData()
 #print outputDictionary
@@ -9,6 +13,14 @@ interpretation = analyzeObj.analyzeData()
 print interpretation
 #all questions with answer have been put in their bins
 # now need to run analysis on text objectsvagrant 
-#JSONObj = JSONUtil()
+JSONObj = JSONUtil()
+
+#now since we have 3 things - scale , opinion and analytics 
+#i shall spawn three threads
+
+try:
+    thread.start_new_thread(JSONObj.generatejson, (outputDictionary,interpretation))
+
+
 #JsonOutput = JSONObj.generateJson(outputDictionary,interpretation)
 #print JSONObj
