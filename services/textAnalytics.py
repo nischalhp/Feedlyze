8276f5650267a1d2a4ef8176cf5fc9a3797ab55d
Nischal HP <n.hp@sap.com>
2014-01-18 23:58:50 +0530

    Modified TA Class to generate basic TA Obj
    * Returns a list of basic TA Obj- sentences,tokens,POSTag,chunks

--- services/textAnalytics.py
+++ services/textAnalytics.py
@@ -1,17 +1,26 @@
 from nltk.tokenize import word_tokenize,wordpunct_tokenize, sent_tokenize 
 from nltk import batch_ne_chunk ,pos_tag
-
+from textAnalyticsBase import TextAnalyticsObj
 # TextAnalyzer
 class TextAnalyzer:
 
 
-
-
-	def __init__(self):
-		self.entities = []
-
-
-
+	def performBasicAnalytics(self,outputDictionary,analyzeData):
+		TA_Obj_list = []
+		key = 'analytics'
+		questions = analyzeData[key]
+		for question in questions:
+			answers = outputDictionary[question]
+			print question,answers
+			TAObj = self.analyzeText(answers)
+			TA_Obj_list.append(TAObj)	
+			#print TAObj.sentences,TAObj.tokens,TAObj.postags,TAObj.chunks
+		# 	analyzedJson = getAnalyzedJson.AnalyzedJson(key,question,dict_of_answers) 	
+		# 	analyzedJson = analyzedJson.toJson()
+		# 	#print analyzedJson
+		# 	inner_json_list.append(analyzedJson)
+		# inner_json = json.dumps({'data':inner_json_dict})	
+		return TA_Obj_list
 
 	def analyzeText(self,answers):
 		#does tokenization , pos tagging , chunking and returns all 3 of them
@@ -27,35 +36,9 @@ class TextAnalyzer:
 				postags = [pos_tag(token) for token in tokens]
 				#chunking
 				chunks = batch_ne_chunk(postags,binary=True)
-				print type(chunks),"chunks type"
-				
-			
-    
-
-	###### ENTITY EXTRACTOR ##########
-
-
-	def entityExtraction(self,chunks):
-		#find entites
-				entites = []
-				for tree in chunks:
-					#print tree,"tree"
-					self.extractEntities(tree)
-					
-				print self.entities
-
-	def extractEntities(self,tree):
-	if hasattr(tree, 'node') and tree.node:
-		#print tree.node,"tree node"
-		if tree.node == 'NE':
-			for child in tree:
-				self.entities.append(' '.join([child[0] for child in tree]))
-				#print child,"child"
-		else:
-			for child in tree:
-				#print child,"recursive child"
-				self.extractEntities(child)
-
+				TAObj = TextAnalyticsObj(sentences,tokens,postags,chunks)
+				return TAObj
+				#print type(chunks),"chunks type"
 
 
 
