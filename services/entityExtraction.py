8107883d5d837c9b66083443d664c5116e967646
Nischal HP <n.hp@sap.com>
2014-01-18 23:51:52 +0530

    Added class for entity extractor

---
+++ services/entityExtraction.py
@@ -0,0 +1,31 @@
+class EntityExtractor:
+    
+
+    ###### ENTITY EXTRACTOR ##########
+    chunks = []
+    entities = []
+
+    def __init__(self,chunks):
+        self.chunks = chunks
+        self.entities = []
+
+
+    def entityExtraction(self):
+        #find entites
+        for tree in self.chunks:
+            #print tree,"tree"
+            self.extractEntities(tree)       
+        print self.entities
+
+    def extractEntities(self,tree):
+        
+        if hasattr(tree, 'node') and tree.node:
+            #print tree.node,"tree node"
+            if tree.node == 'NE':
+                for child in tree:
+                    self.entities.append(''.join([child[0] for child in tree]))
+                    #print child,"child"
+            else:
+                for child in tree:
+                    #print child,"recursive child"
+                    self.extractEntities(child)
